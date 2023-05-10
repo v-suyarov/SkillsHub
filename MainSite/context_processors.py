f2e3f6f935b2bd07f7ae8404f8f32fd44913b43c
v-suyarov <90338171+v-suyarov@users.noreply.github.com>
2023-05-10 16:13:26 +0300

    Revert "Merge branch 'dev' into nastya"
    This reverts commit 1e6c1cc18fa4ce0f450ac6f23ee6218541edd649, reversing
    changes made to a16039bc0725ef03d3701978fb32b3cf976ef067.

--- MainSite/context_processors.py
+++
@@ -1,6 +0,0 @@
-from django.conf import settings
-from MainSite.models import CustomUser
-
-
-def custom_user(request):
-    return {'user': CustomUser.objects.get(id=request.user.id)} if request.user.is_authenticated else {}
